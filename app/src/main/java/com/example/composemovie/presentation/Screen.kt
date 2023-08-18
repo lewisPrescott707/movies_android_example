c8ff0d06a4476fb8b807303b7f84489e64866251
tamerlankayak <tamerlan_94@mail.ru>
2023-08-18 11:23:22 +0400

    test comment

--- app/src/main/java/com/example/composemovie/presentation/Screen.kt
+++ app/src/main/java/com/example/composemovie/presentation/Screen.kt
@@ -1,5 +1,5 @@
 package com.example.composemovie.presentation
-
+//Route
 sealed class Screen(val route: String) {
     object MovieScreen : Screen("movie_screen")
     object MovieDetailScreen : Screen("movie_detail_screen")
