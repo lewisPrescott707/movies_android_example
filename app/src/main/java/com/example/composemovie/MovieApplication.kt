5fe7d2ad75c84eea7b038ed10c935bb65ce3a129
Tamerlan Mustafayev <tamerlankayak@gmail.com>
2023-08-23 12:11:04 +0400

    Update MovieApplication.kt

--- app/src/main/java/com/example/composemovie/MovieApplication.kt
+++ app/src/main/java/com/example/composemovie/MovieApplication.kt
@@ -3,6 +3,7 @@ package com.example.composemovie
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 //1st creating Application class and declaring annotations
+//It is important for Dependency Injection
 @HiltAndroidApp
 class MovieApplication : Application() {
 }
