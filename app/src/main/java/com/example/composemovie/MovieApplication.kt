7bfe61cdc109cebc41060b78716222f67dc11634
Tamerlan Mustafayev <tamerlankayak@gmail.com>
2023-08-23 12:10:19 +0400

    Update MovieApplication.kt

--- app/src/main/java/com/example/composemovie/MovieApplication.kt
+++ app/src/main/java/com/example/composemovie/MovieApplication.kt
@@ -2,7 +2,7 @@ package com.example.composemovie
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-
+//1st creating Application class and declaring annotations
 @HiltAndroidApp
 class MovieApplication : Application() {
 }
