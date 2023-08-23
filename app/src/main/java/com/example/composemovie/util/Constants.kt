09e5d3089e1ab978e275b0eefa528635823fde89
Tamerlan Mustafayev <tamerlankayak@gmail.com>
2023-08-23 12:15:01 +0400

    Update Constants.kt

--- app/src/main/java/com/example/composemovie/util/Constants.kt
+++ app/src/main/java/com/example/composemovie/util/Constants.kt
@@ -2,6 +2,7 @@ package com.example.composemovie.util
 
 
 object Constants {
+    //APİ_KEY yuo can get from www.omdbapi.com also
     const val API_KEY = "bee8132b"
     const val BASE_URL = "http://www.omdbapi.com/"
     const val IMDB_ID = "imdb_id"
